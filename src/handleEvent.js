35a3d3e9a35ce064e0202fcab181b8727befec7b
Crazyglue <edbz2k2@gmail.com>
2020-01-30 20:15:18 -0600

    fix message

--- src/handleEvent.js
+++ src/handleEvent.js
@@ -22,9 +22,9 @@ module.exports = async function handleEvent({ event, authed_users = [] }, callba
         const sanitizedMessage = removedUsers.replace('meme', '').trim();
         const { text: title, imageUrl } = await fetchRedditMeme(sanitizedMessage);
         if (!title || !imageUrl) {
-            message.text = `:ohno: Sorry <@${event.user}> couldn't find any memes :ohno:`
+            message.text = `:ohno: Sorry <@${event.user}> couldn't find any memes :ohno:`;
         }
-        message.text = `:party_dank:${title}:party_dank:\n${imageUrl}`;
+        message.text = `:party_dank: ${title} :party_dank: \n${imageUrl}`;
     } else if (curseRegex.test(event.text)) {
         message.text = `<@${event.user}> thats very rude, why would you say that?`;
     }
