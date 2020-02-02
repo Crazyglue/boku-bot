4af0e547eb9418998668d81c6c7f0c85423eb0f7
Crazyglue <edbz2k2@gmail.com>
2020-02-02 12:58:35 -0600

    round out coverage

--- src/createMeme/parseInputText.ts
+++ src/createMeme/parseInputText.ts
@@ -2,6 +2,6 @@ const quoteRegex = /(["'])(?:(?=(\\?))\2.)*?\1/g;
 
 const replaceMacQuotes = (text: string) => text.replace(/“|”/g, '"');
 
-export default function parseInputText(text: string = ''): string[] {
-    return replaceMacQuotes(text).match(quoteRegex).map((t) => t.replace(/"/g, ''));
+export default function parseInputText(text: string): string[] {
+    return replaceMacQuotes(text).match(quoteRegex).map((t) => t.replace(/"/g, ''))
 };
